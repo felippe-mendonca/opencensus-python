7fabf26a48d9951abdfcd0e1d48b7aeaaccfba5b
Felippe <mendonca.felippe@gmail.com>
2018-08-02 11:38:28 +0100

    version 0.1.5.1 released

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires = [
 
 setup(
     name='is_opencensus',
-    version='0.1.5',
+    version='0.1.5.1',
     author='OpenCensus Authors',
     author_email='census-developers@googlegroups.com',
     classifiers=[
